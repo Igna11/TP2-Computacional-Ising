a40d91bb9d9063d99d1876d5f53378febfc1db6a
Igna11 <ignaao@gmail.com>
2019-05-24 16:23:27 -0300

    asd

--- Ejercicio 2/Ising2D_Ej2b.c
+++ Ejercicio 2/Ising2D_Ej2b.c
@@ -14,21 +14,14 @@ int Flip(int* red, int dim, int muestreos, double B, double T, double J);
 
 int main()
 {
-	//int i, j, k;
-	int *red, dim, muestreos;
-	double p, B, T, J;
+	int *red, dim, muestreos,i ;
+	double M, B, T, J, m_suma, m;
 	
 	printf("\nDame dimension de la red\n");
 	scanf("%i", &dim);
-		
-	printf("\nDame una probabilidad para distribuir uniformemente spines up\n");
-	scanf("%lf", &p);
 	
-	printf("\nDame una intensidad de campo magnetico B\n");
-	scanf("%lf", &B);
-	
-	printf("\nDame una intensidad del acoplamiento J\n");
-	scanf("%lf", &J);
+	printf("\nDame una temperatura T\n");
+	scanf("%lf", &T);
 	
 	printf("\nDame el numero de veces que queres barrer la red\n");
 	scanf("%i", &muestreos);
@@ -38,30 +31,43 @@ int main()
 	
 	srand(time(NULL));
 	
+	//valores que pide el problema: B = 0, inicializo M = 0 y poblar con p = 0.5
+	M = 0.0;
+	B = 0.0;	
+	poblar(red, 0.5, dim);
+
 	FILE* fp;
 	
 	//forma cool de definir el nombre del .txt, para favorecer orden
 	char filename[64];
 	
-	sprintf(filename, "JyB_MvsT_dim%i_J%.1lf_B%.1lf_Pasos%i.txt", dim,J, B, muestreos);
-	
-	double M = 0.0;
+	sprintf(filename, "MvsJ_dim%i_T%.2lf_Pasos_prom%i.txt", dim, T, muestreos);
 	
 	fp = fopen(filename, "w");
-	poblar(red, p, dim);
 	
-	for(T = 10; T > 0.00001; T = T - 0.01)
+	//para medir el tiempo
+	clock_t start, end;
+	double cpu_time_used;
+    start = clock();
+	
+	for(J = 0.1; J < 0.6; J = J + 0.0005)
 	{	
-		//imprimir(red, dim);
-		M = Flip(red, dim, muestreos, B,T,J);
+		m_suma = 0.0;
+		for(i = 0; i < 200; i++)
+		{
+			M = Flip(red, dim, muestreos, B,T,J);
+			m_suma += M;
+		}
+		m = m_suma/(dim*dim*200);
+		fprintf(fp,"%lf\t%lf\n",J,m);
+		printf("J = %lf\t M = %lf\n", J,m);
 		
-		fprintf(fp,"%lf\t%lf\n",T,M);
-		printf("T = %lf\t M = %lf\n", T,M);
-		//printf("\n");
-		//imprimir(red, dim);
 	}
 	fclose(fp);
 	free(red);
-
+	
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("tiempo = %lf\n", cpu_time_used);
 	return 0;
 }
