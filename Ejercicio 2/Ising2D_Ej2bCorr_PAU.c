8d314da49f91c2df681a2074243a2e3af9bf4b4c
Igna11 <ignaao@gmail.com>
2019-05-27 00:24:15 -0300

    asd

--- Ejercicio 2/Ising2D_Ej2bCorr_PAU.c
+++ Ejercicio 2/Ising2D_Ej2bCorr_PAU.c
@@ -5,19 +5,20 @@
 #include <math.h>
 #include <time.h>
 #include "Poblar.h"
-#include "Imprimir.h"
 #include "FlipBJ.h"
 #include "MedVal_d.h"
-#include "M_corr.h"
+#include "M_corr_pau.h"
+
 
 int poblar(int *red, double p, int dim);
-int imprimir(int* red, int dim);
 int Flip(int* red, int dim, double B, double T, double J);
+double M_corr_pau(double* mag, double* c_k, int* red, int dim, double B, double T, double J, int muestreos, int k_max);
+
 
 int main()
 {
-	int *red, dim, muestreos, promedios, i, k;
-	double *mag, *mag_prom, B, T, J, M;
+	int *red, dim, muestreos, promedios, i, k, k_max;
+	double *mag, *c_k, *c_kprom, B, T, J;
 	
 	printf("\nDame dimension de la red\n");
 	scanf("%i", &dim);
@@ -32,64 +33,73 @@ int main()
 	scanf("%i", &promedios);
 	printf("\n\n");
 	
+	k_max = muestreos/2;
+	
 	red = (int*)malloc(dim*dim*sizeof(int));
 	mag = (double*)malloc(muestreos*sizeof(double));
-	mag_prom = (double*)malloc(muestreos*sizeof(double));
+	c_k = (double*)malloc(k_max*sizeof(double));
+	c_kprom = (double*)malloc(k_max*sizeof(double));
 	srand(time(NULL));
 	
 	//valores que pide el problema: B = 0, inicializo M = 0 y poblar con p = 0.5
 	
-	B = 0.0;
-	M = 0.0;
+	B = 0.0;	
+	poblar(red, 0.5, dim);
+
+	//FILE* fp;
 	
 	//forma cool de definir el nombre del .txt, para favorecer orden
-	FILE *fp;
-	char filename[64];
+	//char filename[64];
 	
-	sprintf(filename, "CorvsK_dim%i_Pasos%i_prom%i.txt", dim, muestreos, promedios);
+	//sprintf(filename, "CorvsK_dim%i_Pasos%i_prom%i.txt", dim, muestreos, promedios);
 	
-	fp = fopen(filename, "w");
+	//fp = fopen(filename, "w");
 	
 	//para medir el tiempo
 	clock_t start, end;
 	double cpu_time_used;
     start = clock();
 	
-	printf("nombre de archivo: %s\n", filename);
-	for(J = 0.1; J < 0.6; J = J + 0.01)
-	{
-		//reinicio mag_prom
-		for(i = 0; i < muestreos; i++)
+	for(J = 0.1; J < 0.6; J = J + 0.001)
+	{	
+		//fprintf(fp,"%lf\t", J);
+		printf("J = %lf\n", J);
+		
+		k = 0;
+		//limpio c_k y c_kprom
+		for(k = 0; k < k_max; k++)
 		{
-			mag_prom[i] = 0;
+			c_kprom[k] = 0;
+			c_k[k] = 0;
+			//printf("indice %i borro\n", k);
 		}
 		
-		for(k = 0; k < promedios; k++)
+		for(i = 0; i < promedios; i++)
 		{
-			poblar(red, 0.5, dim);
-			for(i = 0; i < muestreos; i++)
+			M_corr_pau(mag, c_k, red, dim, B, T, J, muestreos, k_max);
+		
+			for(k = 0; k < k_max; k++)
 			{
-				//calculo la magnetización para un dado muestreo.
-				M = Flip(red, dim, B, T, J);
-				mag[i] = M;
+			//fprintf(fp,"%lf ", c_kprom[k]);
+				printf("c_k[%i] = %lf\t ", k, c_k[k]);
 			}
-			
-			for(i = 0; i < muestreos; i++)
+			/*
+			for(k = 0; k < k_max; k++)
 			{
-				mag_prom[i] += mag[i]/(double)promedios;
+				c_kprom[k] += c_k[k];
 			}
+			*/
 		}
-		fprintf(fp,"%lf\t", J);
-		printf("J = %lf\n", J);
-		for(i = 0; i < muestreos; i++)
-		{
-			fprintf(fp,"%lf ", mag_prom[i]);
-		}
-		fprintf(fp,"\n");
+		
+		
+		//fprintf(fp,"\n");
+		//printf("\n");
 	}
+	//fclose(fp);
 	free(red);
 	free(mag);
-	free(mag_prom);
+	free(c_k);
+	free(c_kprom);
 	
 	end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
--- Ejercicio 2/M_corr.h
+++ Ejercicio 2/M_corr.h
@@ -1,29 +1,23 @@
 #pragma once
-int Flip(int* red, int dim, double B, double T, double J);
 double mean(double* vec, int iteraciones);
 double pseudomean(double* vec, int len_ck, int k);
 double mean_xcuad(double* vec, int iteraciones);
 
-double M_corr(double* mag, double* c_k, int* red, int dim, double B, double T, double J, int muestreos, int k_max)
+double M_corr(double* mag, double* c_k, int k_max)
 {
-	double M, M_mean, M2_mean, M_mean2, MK_mean, result;
-	int i;
-	
-	for(i = 0; i < muestreos; i++)
-	{
-		M = Flip(red, dim, B, T, J);
-		mag[i] = M;
-	}
+	double M_mean, M2_mean, M_mean2, MK_mean, result;
+	int k;
+
 	//calculo los valores medios:
 	M_mean = mean(mag, k_max);
 	M2_mean = mean_xcuad(mag, k_max);
 	M_mean2 = M_mean*M_mean;
 		
-	for(i = 0; i < k_max; i++)
+	for(k = 0; k < k_max; k++)
 	{
-		MK_mean = pseudomean(mag, k_max, i);
+		MK_mean = pseudomean(mag, k_max, k);
 		result = (MK_mean - M_mean2)/(M2_mean - M_mean2);
-		c_k[i] = result;
+		c_k[k] = result;
 	}
 	return 0;
 }
