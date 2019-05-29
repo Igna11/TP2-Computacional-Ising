26c041f73bf815ff43695eb4d5853cc6dceb6605
Paula <paulacaral@hotmail.com>
2019-05-29 20:14:48 -0300

    Chi y Calor corregidos + .c Para item (c)

---
+++ Ejercicio 2/FlipBJ_c.h
@@ -0,0 +1,48 @@
+#pragma once
+int Flip(int* red, int dim, double B, double J, double E_suma, double* E) // de ahora en mas, B y J son B* y J*. asi que borré T.
+{
+	int i, j, suma, indice_acop, indice_mag, S_arriba, S_abajo, S_izq, S_der, S;
+	double P_aceptacion, rnd, M;
+	
+	double p_acop[5] = {exp(8*J), exp(4*J), 1, exp(-4*J), exp(-8*J)};
+	double p_mag[2] = {exp(2*B), exp(-2*B)};
+	
+	M = 0.0;
+	P_aceptacion = 0.0;
+	for(j = 0; j < dim; j++)
+	{
+		for(i = 0; i < dim; i++)
+		{
+			//el numero random entre 0 y 1
+			rnd = (double)rand()/(double)RAND_MAX;
+			
+			//chequeado 23-05-2019 y funciona!
+			S_arriba = red[((j - 1 + dim)%dim)*dim + i];
+			S_abajo  = red[((j + 1 + dim)%dim)*dim + i];
+			S_izq    = red[j*dim + (i - 1 + dim)%dim];
+			S_der    = red[j*dim + (i + 1 + dim)%dim];
+			
+			S = red[j*dim + i];
+			
+			
+			suma = S_abajo + S_arriba + S_der + S_izq;
+			
+			indice_mag = (S + 1)/2;
+			indice_acop = (S*suma + 4)/2;
+
+			P_aceptacion = p_acop[indice_acop]*p_mag[indice_mag];
+
+			if(P_aceptacion > rnd) red[j*dim + i] = -S;
+			
+			E_suma += S*S_arriba + S*S_abajo + S*S_izq + S*S_der;
+		}			
+	}
+	
+	E[0] = E_suma;	
+
+	for(i = 0; i < dim*dim; i++)
+	{
+		M += red[i];
+	}
+	return M;
+}
---
+++ Ejercicio 2/Ising2D_Ej2c.c
@@ -0,0 +1,85 @@
+/* gcc -Wall -O3 -o Ising2D_Ej2b.exe Ising2D_Ej2b.c -lm*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "Poblar.h"
+#include "FlipBJ_c.h"
+
+int poblar(int *red, double p, int dim);
+int Flip(int* red, int dim, double B, double J, double E_suma, double* E);
+
+int main()
+{
+	int *red, dim, muestreos, k, tau, i;
+	double M, B, J, tiempo, E_suma, *E;
+	
+	printf("\nDame dimension de la red\n");
+	scanf("%i", &dim);
+		
+	printf("\nDame el numero de veces que queres barrer la red\n");
+	scanf("%i", &muestreos);
+	
+	printf("\nDame el tau\n");
+	scanf("%i", &tau);
+	
+	red = (int*)malloc(dim*dim*sizeof(int));
+	
+	srand(time(NULL));
+	
+	//valores que pide el problema: B = 0, inicializo M = 0 y poblar con p = 0.5
+	M = 0.0;
+	B = 0.0;
+	E_suma = 0.0;
+	E = (double*)malloc(1*sizeof(double));
+	poblar(red, 0.5, dim);
+
+	FILE* fp;
+	FILE* fpE;
+	
+	//forma cool de definir el nombre del .txt, para favorecer orden
+	char filenameE[64];
+	char filename[64];
+	
+	sprintf(filenameE, "EvsJ_dim%i_Barridos%i.txt", dim, muestreos);
+	sprintf(filename, "MvsJ_dim%i_Barridos%i.txt", dim, muestreos);
+	
+
+	fpE = fopen(filenameE,"w");
+	fp = fopen(filename, "w");
+		
+	//para medir el tiempo
+	clock_t start, end;
+	double cpu_time_used;
+    start = clock();
+	
+	tiempo = dim*dim*muestreos*3500/280000000;
+	printf("\nEsto va a tardar aprox %.0lf segundos o %.0lf minutos\n", tiempo ,tiempo/60);
+	for(J = 0.1; J < 0.6; J = J + 0.01)
+	{	
+		E_suma = 0.0;
+		fprintf(fp,"%lf\t",J);
+		fprintf(fpE,"%lf\t",J);
+		for(i = 0; i< muestreos; i++)
+		{
+			for(k = 0; k < tau; k++)
+			{
+				M = Flip(red, dim, B, J, E_suma, E);
+			}
+			fprintf(fp,"%lf\t", M);
+			fprintf(fpE,"%lf\t", E[0]);
+		}
+		fprintf(fp,"\n");
+		fprintf(fpE,"\n");
+		printf("Termine con J = %lf hice %i M's \n", J, i);
+	}
+	fclose(fp);
+	fclose(fpE);
+	free(red);
+	
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("tiempo = %lf\n", cpu_time_used);
+	return 0;
+}
